5fcf483f529d4b5ec83b59f49d087e3448fe1c0a
Rob Flickenger <rob@hackerfriendly.com>
2022-09-17 19:13:27 -0700

    Minimal install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git clone https://github.com/CompVis/taming-transformers.git
+git clone https://github.com/openai/CLIP.git
+
+echo
+echo "Download the Stable Diffusion model and install it to models/ldm/stable-diffusion-v1/model.ckpt"
+echo "It can be found at https://huggingface.co/CompVis/stable-diffusion-v1-4"
+echo
+
